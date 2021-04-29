50e8d009b4891b7227e0393e6c5eefdd66943cb6
xdeejm97 <d.niesmialowski@gmail.com>
2021-04-29 23:09:38 +0200

    next

--- src/com/sda/pętle/Petla_kolejne.java
+++ src/com/sda/pętle/Petla_kolejne.java
@@ -1,2 +1,12 @@
-package com.sda.pętle;public class Petla_kolejne {
+package com.sda.pętle;
+
+public class Petla_kolejne {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
 }
