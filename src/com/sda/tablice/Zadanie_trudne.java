eb937fd4807d9a99f970356a6db8c2fad2a37aba
xdeejm97 <d.niesmialowski@gmail.com>
2021-04-29 22:44:44 +0200

    New Task

---
+++ src/com/sda/tablice/Zadanie_trudne.java
@@ -0,0 +1,25 @@
+package com.sda.tablice;
+
+public class Zadanie_trudne {
+    //jest to zadanie z jednego madrego kursu, z ktorego uczylem sie problem solvingu.
+//polecenie brzmi: napisz metode, ktora zwraca najczescie wystepujacy element tablicy
+//tablica: 40 elementow, losowych, liczby int od 1 do 12
+   // public static void main(String[] args) {
+   //     // NOTE: The following input values are used for testing your solution.
+   //     // mostFrequent(array1) should return 1.
+   //     int[] array1 = {1, 3, 1, 3, 2, 1};
+   //     // mostFrequent(array2) should return 3.
+   //     int[] array2 = {3, 3, 1, 3, 2, 1};
+   //     // mostFrequent(array3) should return null.
+   //     int[] array3 = {};
+   //     // mostFrequent(array4) should return 0.
+   //     int[] array4 = {0};
+   //     // mostFrequent(array5) should return -1.
+   //     int[] array5 = {0, -1, 10, 10, -1, 10, -1, -1, -1, 1};
+   //     System.out.println(mostFreqent(array1));
+   //     System.out.println(mostFreqent(array2));
+   //     System.out.println(mostFreqent(array3));
+   //     System.out.println(mostFreqent(array4));
+   //     System.out.println(mostFreqent(array5));
+   // }
+}
