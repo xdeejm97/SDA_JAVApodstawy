49d6b31400350b161b8f8e47658046c3f2729185
xdeejm97 <d.niesmialowski@gmail.com>
2021-04-29 23:42:15 +0200

    changes

--- src/com/sda/pętle/Petla_kolejne.java
+++ src/com/sda/pętle/Petla_kolejne.java
@@ -3,7 +3,7 @@ package com.sda.pętle;
 public class Petla_kolejne {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            if (i == 4) {
+            if (i == 9) {
                 break;
             }
             System.out.println(i);
