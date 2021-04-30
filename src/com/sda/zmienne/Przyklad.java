ab87f3b0df797ae242c1e7aa6a54d3d181ff0c80
xdeejm97 <d.niesmialowski@gmail.com>
2021-04-30 12:58:31 +0200

    powtorka

---
+++ src/com/sda/zmienne/Przyklad.java
@@ -0,0 +1,10 @@
+package com.sda.zmienne;
+
+public class Przyklad {
+    class ExampleClass{
+        int myGlobal = 12;
+        void someMethod (){
+            System.out.println("My variable global is: " + myGlobal);
+        }
+    }
+}
---
+++ src/com/sda/zmienne/Przyklad_substring.java
@@ -0,0 +1,18 @@
+package com.sda.zmienne;
+
+public class Przyklad_substring {
+    public static void main(String[] args) {
+
+
+    String hello = "Witaj ";
+    String world = "Świecie!";
+    String powitanie = hello + world; //łączenie dwóch napisów
+        System.out.println(powitanie);
+
+    String czesc = powitanie.substring(0, 6) + "uczniu";
+        System.out.println(czesc);
+        int a = 10;
+        a += 2;
+        System.out.println(a);
+}
+}
