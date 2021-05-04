187a1929b29f5e565416224904370e632c5d1186
xdeejm97 <d.niesmialowski@gmail.com>
2021-05-04 15:34:17 +0200

    stare zmiany

--- src/com/sda/zmienne/Przyklad_dodacMaina.java
+++ src/com/sda/zmienne/Przyklad_dodacMaina.java
@@ -1,5 +1,13 @@
 package com.sda.zmienne;
 
-public class Przyklad {
-    
+public class Przyklad_dodacMaina {
+    class ExampleClass{
+        int myGlobal = 12;
+            void someMethod (){
+                System.out.println("My variable global is: " + myGlobal);
+    }
+}
+
+
+
 }
--- src/com/sda/zmienne/Substring.java
+++ src/com/sda/zmienne/Substring.java
@@ -1,2 +1,19 @@
-package com.sda.zmienne;public class Substring {
+package com.sda.zmienne;
+
+public class Substring {
+    public static void main(String[] args) {
+
+        String hello = "Witaj ";
+        String world = "Świecie!";
+        String powitanie = hello + world; //łączenie dwóch napisów
+        System.out.println(powitanie);
+
+        String czesc = powitanie.substring(0, 6) + "uczniu";
+        System.out.println(czesc);
+        int a = 10;
+        a += 2;
+        System.out.println(a);
+    }
+
+
 }
